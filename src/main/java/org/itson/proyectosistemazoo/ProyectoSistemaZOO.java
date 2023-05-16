030c0978cc4de242e8df31268b914ca9287438f4
Kim Serrano Uribe <alejandra.ser.ur@gmail.com>
2023-05-16 15:38:34 -0700

    prueba

--- src/main/java/org/itson/proyectosistemazoo/ProyectoSistemaZOO.java
+++ src/main/java/org/itson/proyectosistemazoo/ProyectoSistemaZOO.java
@@ -12,6 +12,6 @@ package org.itson.proyectosistemazoo;
 public class ProyectoSistemaZOO {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! hola");
     }
 }
