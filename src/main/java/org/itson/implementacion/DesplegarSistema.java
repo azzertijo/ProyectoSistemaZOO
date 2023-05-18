6c7b6a86ea8f6ced6d9ea403fe002c2cc11d6477
azzertijo <azzertijo@outlook.com>
2023-05-18 14:36:52 -0700

    Clase para desplegar el sistema
    despliega el sistema para iniciar

---
+++ src/main/java/org/itson/implementacion/DesplegarSistema.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.itson.implementacion;
+
+import org.itson.GUI.FrmGuia;
+
+/**
+ * Clase donde se implementa un método para iniciar el sistema
+ * @author kim, marki, elmer, yorx
+ */
+public class DesplegarSistema {
+    
+    /**
+     * Método que despliega la ventana de inicio del sistema
+     */
+    public void desplegarSistema(){
+        FrmGuia frmGuia = new FrmGuia();
+        frmGuia.setVisible(true);
+    }
+    
+}
